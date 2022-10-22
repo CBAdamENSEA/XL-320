f8515e8a56e8aa22ef545972e4b1027289c61752
CBAdamENSEA <adam.cheikh-brahim@ensea.fr>
2022-10-22 20:35:10 +0200

    redirecting printf

--- XL-320/Core/Src/stm32f7xx_hal_msp.c
+++ XL-320/Core/Src/stm32f7xx_hal_msp.c
@@ -37,12 +37,16 @@ extern DMA_HandleTypeDef hdma_usart1_rx;
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN Macro */
-
+#ifdef __GNUC__ /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf    set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif /* __GNUC__ */
 /* USER CODE END Macro */
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN PV */
-
+extern UART_HandleTypeDef huart1;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -243,6 +247,18 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
 }
 
 /* USER CODE BEGIN 1 */
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART2 and Loop until the end of transmission */
+  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 0xFFFF);
 
+  return ch;
+}
 /* USER CODE END 1 */
 
